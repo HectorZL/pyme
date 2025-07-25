4d55782b39a0554e3787ca081b2bf699211d868f
Hector Zambrano <zambranohector2002@gmail.com>
2025-07-25 17:05:01 -0500

    <fix>[change the footer ,add links ,delete the toogle of dark mode]: <description>
    [optional body]
    
    [optional footer(s)]

--- core/settings.py
+++ core/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -65,8 +66,20 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME', 'defaultdb'),
+        'USER': os.getenv('DB_USER', 'avnadmin'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'AVNS_uaFmMzm-XdnoIfDCxyz'),
+        'HOST': os.getenv('DB_HOST', 'mysql-3aea397e-jesucitozl-45ba.g.aivencloud.com'),
+        'PORT': os.getenv('DB_PORT', '10199'),
+        'OPTIONS': {
+            'ssl': {
+                'ssl-mode': 'REQUIRED',
+                'ssl_ca': None,  # Add CA certificate path if needed
+            },
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
@@ -93,13 +106,24 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'pt'
+LANGUAGE_CODE = 'es-es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Lima'
 
 USE_I18N = True
 
+USE_L10N = True
+
 USE_TZ = True
+
+# Languages available for translation
+LANGUAGES = [
+    ('es', 'Español'),
+]
+
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'locale'),
+]
 
 
 # Static files (CSS, JavaScript, Images)
